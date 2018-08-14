af68af156c0f63010d14253f67b6256be24726ca
imprashanthv <vissaprashanth@gmail.com>
2018-08-14 14:38:26 +0530

    made some car changes

--- explanations.js
+++ explanations.js
@@ -1,1 +1,30 @@
 //Explanations
+var favColor = 'blue';
+var myFavColor = ['blue', 'red', 'green'];
+var numOfFavColors = '3';
+var hasGotFavColors = true;
+var prashanthObject = {
+	firstName: 'Prashanth',
+	lastName: 'Vissapragada',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 22,
+	isMale: true
+};
+
+var whatIsMyFavColor = function(){
+	return 'blue';
+}
+
+var area = function(width,height){
+	return width*height;
+}
+
+whatIsMyFavColor();
+area(4,3);
+
+if(prashanthObject.isMale && whatIsMyFavColor()=='blue'){
+	alert('true');
+}
+else{
+	alert('false');
+}
